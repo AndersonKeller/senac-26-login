db44e3c738d5f87637789da276c30e167635aa25
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2026-08-27 19:44:37 -0300

    Add files via upload

--- login/script.js
+++ login/script.js
@@ -26,8 +26,31 @@ function formLogin() {
             }
             const userJson = JSON.stringify(user)//JSON
             localStorage.setItem("user", userJson)
-            location.href = "/"
+            montarModal("Login efetuado com sucesso!")
+            setTimeout(() => {
+
+                location.href = "/"
+            }, 3000);
+        } else {
+            montarModal("Credenciais inválidas")
         }
     })
 }
 formLogin()
+//MODAL
+function montarModal(mensagem) {
+    const body = document.querySelector("body")
+    body.insertAdjacentHTML("beforeend", `
+        <div class="wrapper">
+        <div class="modal">
+            <p>${mensagem}</p>
+            <button id="close">Tentar novamente</button>
+        </div>
+    </div>
+        `)
+    const btnClose = document.querySelector("#close")
+    btnClose.addEventListener("click", () => {
+        const wrapper = document.querySelector(".wrapper")
+        wrapper.remove()
+    })
+}
